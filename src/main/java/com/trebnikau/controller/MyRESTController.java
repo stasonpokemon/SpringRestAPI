81638c73aa5eee7213ad907d09b0088d7df63c18
Stanislau Trebnikau <Stanislau_Trebnikau@epam.com>
2022-05-20 02:15:28 +0300

    add global exception handler and @ControllerAdvice

--- src/main/java/com/trebnikau/controller/MyRESTController.java
+++ src/main/java/com/trebnikau/controller/MyRESTController.java
@@ -33,17 +33,5 @@ public class MyRESTController {
         return employee;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSUchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
 }
